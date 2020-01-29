8748d15ac60d83899fbe8e7be9fc93f5be260e9f
KIRK WALLS <KIRWALLS@uat.edu>
2020-01-29 13:07:21 -0700

    Added AI moving states

--- Assets/Scripts/SimpleAIController.cs
+++ Assets/Scripts/SimpleAIController.cs
@@ -17,6 +17,17 @@ public class SimpleAIController : MonoBehaviour
     public int currentWaypoint = 0;
 
     public float closeEnough = 1.0f;
+
+    public bool isPatrolForward = true;
+
+    public enum LoopType
+    {
+        Stop,
+        Loop,
+        PingPong
+    };
+
+    public LoopType loopType = LoopType.Stop;
     // Start is called before the first frame update
     void Start()
     {
@@ -40,10 +51,63 @@ public class SimpleAIController : MonoBehaviour
         // If we are close to the waypoint,
         if (Vector3.SqrMagnitude(waypoints[currentWaypoint].position - tf.position) < (closeEnough * closeEnough))
         {
-            // Advance to the next waypoint, if we are still in range
-            if (currentWaypoint < waypoints.Length - 1)
+            switch (loopType)
             {
-                currentWaypoint++;
+                case LoopType.Stop:
+
+                // Advance to the next waypoint, if we are still in range
+                    if (currentWaypoint < waypoints.Length - 1)
+                    {
+                        currentWaypoint++;
+                    }
+
+                break;
+
+                case LoopType.Loop:
+                    if (currentWaypoint < waypoints.Length - 1)
+                    {
+                        currentWaypoint++;
+                    }
+                    else
+                    {
+                        currentWaypoint = 0;
+                    }
+
+                    break;
+                case LoopType.PingPong:
+                    if (isPatrolForward)
+                    {
+                        // Advance to the next waypoint, if we are still in range
+                        if (currentWaypoint < waypoints.Length - 1)
+                        {
+                            currentWaypoint++;
+                        }
+                        else
+                        {
+                            //Otherwise reverse direction and decrement our current waypoint
+                            isPatrolForward = false;
+                            currentWaypoint--;
+                        }
+                    }
+                    else
+                    {
+                        // Advance to the next waypoint, if we are still in range
+                        if (currentWaypoint > 0)
+                        {
+                            currentWaypoint--;
+                        }
+                        else
+                        {
+                            //Otherwise reverse direction and decrement our current waypoint
+                            isPatrolForward = true;
+                            currentWaypoint++;
+                        }
+                    }
+
+                    break;
+                default:
+                    Debug.LogError("LoopType not implemented.");
+                    break;
             }
         }
     }
